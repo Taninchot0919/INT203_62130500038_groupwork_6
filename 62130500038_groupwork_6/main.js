fa58a7e4e1046213739fecde8f9bded0499566f0
Taninchot Phuwaloetthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-03-03 23:44:53 +0700

    [UPDATE] Do as requirement
    Left CSS and decorate to handle

---
+++ 62130500038_groupwork_6/main.js
@@ -0,0 +1,68 @@
+const constraints = {
+    firstname: {
+        presence: true,
+    },
+    lastname: {
+        presence: true,
+    },
+    email: {
+        presence: true,
+        email: true,
+    },
+    career: {
+        presence: true,
+    },
+    telno: {
+        presence: true,
+        length: {
+            minimum: 10,
+            message: "must be at least 10 digits"
+        },
+    }
+}
+const app = Vue.createApp({
+    data() {
+        return {
+            pictureUrl: '/images/IMG_1615.jpg',
+            firstname: 'Taninchot',
+            lastname: 'Phuwaloertthiwat',
+            email: 'Taninchot@Taninchot.com',
+            career: 'Full Stack Developer',
+            telno: '1234567890',
+            form: {
+                firstname: null,
+                lastname:null,
+                email: null,
+                career: null,
+                telno: null,
+            },
+            errors: null,
+        }
+    },
+    methods: {
+        checkForm(e) {
+            console.log('Checkform Method run')
+            this.errors = validate({
+                firstname: this.form.firstname,
+                lastname : this.form.lastname,
+                career: this.form.career,
+                email: this.form.email,
+                telno: this.form.telno,
+            }, constraints)
+            if (this.errors) {
+                console.log(this.errors)
+                e.preventDefault();
+            } if (this.errors == null) {
+                this.firstname = this.form.firstname;
+                this.lastname = this.form.lastname;
+                this.career = this.form.career;
+                this.email = this.form.email;
+                this.telno = this.form.telno;
+                console.log(this.errors)
+                alert('Pass')
+                e.preventDefault();
+            }
+        }
+    },
+})
+app.mount('#app')
